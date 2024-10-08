3e1ca9d1e0dd65e749db079563fd029f837ada26
pharos-lab <1i6h7h0u53.lighthouse@gmail.com>
2024-10-08 09:36:08 +0200

    made table component basic structure

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,60 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>test table</h1>
+
+  <Table :data="data"/>
 </template>
 
+<script setup>
+import Table from './components/Table.vue';
+
+const data = [
+  {
+    name: 'mth',
+    lastName: 'dht',
+    age: 35,
+    active: true
+  },
+  {
+    name: 'mth',
+    lastName: 'dht',
+    age: 35,
+    active: true
+  },
+  {
+    name: 'mth',
+    lastName: 'dht',
+    age: 35,
+    active: true
+  },
+  {
+    name: 'mth',
+    lastName: 'dht',
+    age: 35,
+    active: true
+  },
+  {
+    name: 'mth',
+    lastName: 'dht',
+    age: 35,
+    active: true
+  },
+  {
+    name: 'mth',
+    lastName: 'dht',
+    age: 35,
+    active: true
+  },
+  {
+    name: 'mth',
+    lastName: 'dht',
+    age: 35,
+    active: true
+  }
+]
+</script>
+
+
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ src/components/Table.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="table-wrapper">
+        <p v-if="props.data.length === 0">no data available</p>
+
+        <table v-else>
+            <thead>
+                <tr>
+                    <th v-for="label in Object.keys(props.data[0])">{{ label }}</th>
+                </tr>
+            </thead>
+
+
+            <tbody>
+                <tr v-for="row in props.data">
+                    <td v-for="item in row"> {{ item }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    data: {
+        type: Array,
+        default: () => []
+    },
+
+})
+</script>
