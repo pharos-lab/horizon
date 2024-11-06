fcb57865ce436269e7db11ef5e16a4cd7a2608b6
mthdht <mthdht@gmail.com>
2024-11-06 10:19:37 +0100

    ui: made dropdown design for filters and columns visibility

--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -3,25 +3,26 @@
         <div class="horizon-filters-icon" @click="isOpenFilter = !isOpenFilter">
             <FunnelIcon class="horizon-td-icon size-6 text-slate-500"/>
         </div>
-        <div class="horizon-filters absolute bg-red-100 right-0 top-10 min-w-52 z-50" v-show="isOpenFilter">
+        <div class="horizon-filters absolute right-0 top-10 min-w-52 z-50 p-4 bg-white shadow" v-show="isOpenFilter">
+            <h3 class="font-semibold mb-4">Filters</h3>
             <template v-for="filter in props.filters" :key="filter.label">
-                <div class="horizon-filter-checkbox" v-if="filter.type === 'checkbox'">
-                    <label>
+                <div class="horizon-filter-checkbox mb-4" v-if="filter.type === 'checkbox'">
+                    <label class="flex gap-4">
                         <input type="checkbox" v-model="activeFilters.checkbox[filter.column]" :value="filter.value">
                         {{ filter.label }}
                     </label>
                 </div>
 
-                <div class="horizon-filter-select" v-if="filter.type === 'select'">
-                    <label>{{ filter.label }}</label>
-                    <select v-model="activeFilters.select[filter.column]">
+                <div class="horizon-filter-select mb-4" v-if="filter.type === 'select'">
+                    <label class="mr-4">{{ filter.label }}</label>
+                    <select v-model="activeFilters.select[filter.column]" class="border p-1">
                         <option value="">All</option>
                         <option v-for="option in filter.options" :key="option" :value="option">{{ option }}</option>
                     </select>
                 </div>
             </template>
 
-            <button @click="resetFilters">Reset Filters</button>
+            <button @click="resetFilters" class="rounded bg-slate-300 py-2 px-3">Reset Filters</button>
         </div>
     </div>
 
@@ -30,7 +31,8 @@
             <ViewColumnsIcon class="horizon-td-icon size-6 text-slate-500"/>
         </div>
 
-        <div class="horizon-filters absolute bg-red-100 right-0 top-10 min-w-52 z-50" v-show="isOpenColumn">
+        <div class="horizon-filters absolute right-0 top-10 min-w-52 z-50 p-4 bg-white shadow flex flex-col gap-4" v-show="isOpenColumn">
+            <h3 class="font-semibold mb-4">Columns visibility</h3>
             <template v-for="columnFilter in props.filters.find(filter => filter.type === 'column').columns ">
                 <label>
                     <input type="checkbox" v-model="activeFilters.column[columnFilter]">
